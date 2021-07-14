65a02013e7150975170db7525ff370a4e341395d
Bradley Fisher <brad.fisher25@gmail.com>
2021-07-14 09:22:38 -0500

    Fix bug for trusted advisor where metadata doesn't come in expected order (#11)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v0.1.2
+
+Fix bug for trusted advisor where metadata doesn't come in expected order
+
 ## v0.1.1
 
 Fix bux for multi-role trusted advisor
--- pkg/aws/utils_test.go
+++ pkg/aws/utils_test.go
@@ -2,6 +2,8 @@ package aws
 
 import (
 	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
 )
 
 func TestContains(t *testing.T) {
@@ -34,6 +36,41 @@ func TestContains(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Contains(tt.args.a, tt.args.x); got != tt.want {
 				t.Errorf("Contains() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAwsStringContains(t *testing.T) {
+	type args struct {
+		a []*string
+		x *string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "#1",
+			args: args{a: []*string{aws.String("a"), aws.String("b"), aws.String("c")}, x: aws.String("c")},
+			want: true,
+		},
+		{
+			name: "#2",
+			args: args{a: []*string{aws.String("a"), aws.String("b"), aws.String("c")}, x: aws.String("d")},
+			want: false,
+		},
+		{
+			name: "#3",
+			args: args{a: []*string{}, x: aws.String("d")},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SdkStringContains(tt.args.a, tt.args.x); got != tt.want {
+				t.Errorf("SdkStringContains() = %v, want %v", got, tt.want)
 			}
 		})
 	}
